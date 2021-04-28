8033ec5bee438837f066e140e52502da180710fc
Mpendulo1 <82204710+Mpendulo1@users.noreply.github.com>
2021-04-28 16:27:57 +0200

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,12 +1,17 @@
-def fibonacci(n):
-    if (n < 0):
-        return 'Please give a number that is equal or greater than Zero'
-    elif (n == 0):
-        return 0
-    elif (n <= 2):
-        return 1
-    else:
-        return (fibonacci(n - 2) + fibonacci(n - 1))
-# user must input a number/value
-n = int(input('Fibonacci Of: '))
-print('Is: ', fibonacci(n))
+# define your function
+def fibonacci(n):   
+  # 
+  if n <= 1:
+    return n
+  else:
+    return ( fibonacci(n - 1) +  fibonacci(n - 2))
+#   user must input a value/number
+terms_1 = int(input('number of terms: '))
+#  validating the terms if the run
+if terms_1 <= 0:
+  print('please input a positive number: ')
+else:
+  print('Fibonacci Seq: ')
+# looping through the n range
+for i in range(terms_1):
+    print(fibonacci(i))
